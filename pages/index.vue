57db1361e776a6730ad56ed1c4edd2202f8b0afc
wehbs <shamirwehbe@me.com>
2020-05-24 09:47:09 -0400

    Placed loadingIcon in its own component

--- pages/index.vue
+++ pages/index.vue
@@ -8,85 +8,7 @@
     </div>
     <div v-else class="row min-vh-100">
       <div class="col-auto m-auto">
-        <svg
-          xmlns:svg="http://www.w3.org/2000/svg"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          version="1.0"
-          width="64px"
-          height="64px"
-          viewBox="0 0 128 128"
-          xml:space="preserve"
-        >
-          <rect x="0" y="0" width="100%" height="100%" fill="#f5f5f5" />
-          <g>
-            <circle cx="16" cy="64" r="16" fill="#000000" fill-opacity="1" />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#555555"
-              fill-opacity="0.67"
-              transform="rotate(45,64,64)"
-            />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#949494"
-              fill-opacity="0.42"
-              transform="rotate(90,64,64)"
-            />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#cccccc"
-              fill-opacity="0.2"
-              transform="rotate(135,64,64)"
-            />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#e1e1e1"
-              fill-opacity="0.12"
-              transform="rotate(180,64,64)"
-            />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#e1e1e1"
-              fill-opacity="0.12"
-              transform="rotate(225,64,64)"
-            />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#e1e1e1"
-              fill-opacity="0.12"
-              transform="rotate(270,64,64)"
-            />
-            <circle
-              cx="16"
-              cy="64"
-              r="16"
-              fill="#e1e1e1"
-              fill-opacity="0.12"
-              transform="rotate(315,64,64)"
-            />
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              values="0 64 64;315 64 64;270 64 64;225 64 64;180 64 64;135 64 64;90 64 64;45 64 64"
-              calcMode="discrete"
-              dur="720ms"
-              repeatCount="indefinite"
-            ></animateTransform>
-          </g>
-        </svg>
+        <loadingIcon />
       </div>
     </div>
     <modal />
@@ -97,6 +19,7 @@
 import card from '../components/card.vue'
 import modal from '../components/modal.vue'
 import postSearch from '../components/postSearch.vue'
+import loadingIcon from '../components/loadingIcon.vue'
 const db = firebase.firestore()
 
 export default {
@@ -104,7 +27,8 @@ export default {
   components: {
     card,
     modal,
-    postSearch
+    postSearch,
+    loadingIcon
   },
   data() {
     return {
